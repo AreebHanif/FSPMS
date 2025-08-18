beb1b40d140ac4464ff9052485d01ff20071a387
Areeb hanif <141751751+AreebHanif@users.noreply.github.com>
2025-08-18 14:12:10 +0500

    Ccors corrected

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(cors({
-    origin: "*",
+    origin: "https://ah-pms.netlify.app",
     credentials: true
 }))
 
